c30810146f3714e5e2385071178faae887c5ba48
seohee0112 <ghdtj7@naver.com>
2022-10-11 20:09:05 +0900

    feat : Dropdown이벤트 구현

--- week1/Happhee.log/main.js
+++ week1/Happhee.log/main.js
@@ -6,9 +6,28 @@ const ICON = "expand_more";
 const velogTimeBtn = $(".time__btn");
 const velogTimeList = $$(".time__item");
 
-function selectVelogTime(e) {}
-function toggleVelogTimeMenu() {}
+// 선택한 시간타입을 알아내고 다시 토글 후 select 클래스를 제어해 리스트의 배경색 제어
+function selectVelogTime(e) {
+  const selectTime = e.target.innerText;
+  velogTimeBtn.value = selectTime;
 
+  toggleVelogTimeMenu();
+
+  velogTimeList.forEach((velogTime) => {
+    if (
+      velogTime.classList.contains("select") &&
+      velogTime.textContent != selectTime
+    ) {
+      velogTime.classList.remove("select");
+    } else if (
+      !velogTime.classList.contains("select") &&
+      velogTime.textContent == selectTime
+    ) {
+      velogTime.classList.add("select");
+    }
+  });
+}
+function toggleVelogTimeMenu() {}
 function attachEvent() {
   velogTimeBtn.addEventListener("click", toggleVelogTimeMenu);
   velogTimeList.forEach((velogTime) => {
