d851d33dca808e6f3ef8bcfd876538d18f60ebf5
seohee0112 <ghdtj7@naver.com>
2022-10-11 10:17:37 +0900

    feat : element 조작을 위한 선택자, 매직넘버 상수 선언

---
+++ week1/Happhee-To_do/main.js
@@ -0,0 +1,9 @@
+const $$ = (selector) => document.querySelectorAll(selector);
+const $ = (selector) => document.querySelector(selector);
+const TODAY = 0;
+const TOMORROW = 1;
+
+const INPUT = 0;
+const BUTTON = 1;
+
+const todoLists = $$(".todo__list");
