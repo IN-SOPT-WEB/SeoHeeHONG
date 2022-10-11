d103aa3b594b4bb27168782592bfe640804efd37
seohee0112 <ghdtj7@naver.com>
2022-10-11 10:19:04 +0900

    init : 필요한 함수 선언및 윈도우가 로드되었을때, 이벤트를 부착하여 초기화

--- week1/Happhee-To_do/main.js
+++ week1/Happhee-To_do/main.js
@@ -7,3 +7,28 @@ const INPUT = 0;
 const BUTTON = 1;
 
 const todoLists = $$(".todo__list");
+
+function removeTotoList(e) {}
+
+function addTodoList(e) {}
+
+function selectTodoType(e) {}
+// 이벤트 부착
+function attachEvent({ todoForms, selectTypeBtns }) {
+  todoForms.forEach((todoForm) => {
+    todoForm.addEventListener("submit", addTodoList);
+  });
+  todoLists.forEach((todoList) => {
+    todoList.addEventListener("click", removeTotoList);
+  });
+  selectTypeBtns.forEach((typeBtn) => {
+    typeBtn.addEventListener("click", selectTodoType);
+  });
+}
+
+window.onload = () => {
+  attachEvent({
+    todoForms: $$(".todo__form"),
+    selectTypeBtns: $$(".nav__item"),
+  });
+};
