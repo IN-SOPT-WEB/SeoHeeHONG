ff9bcec4d7280232bb7fdfa475b01665995d35eb
seohee0112 <ghdtj7@naver.com>
2022-10-11 20:14:36 +0900

    feat : 인풋에서 엔터키를 누르고, 중복되지 않는 태그이면 태그로 추가하는 기능 구현

--- week1/Happhee.log/writePage/write.js
+++ week1/Happhee.log/writePage/write.js
@@ -6,7 +6,31 @@ const TAGCONTENT = 1;
 
 function removeTag(e) {}
 
-function addInputTag(e) {}
+function addInputTag(e) {
+  const tagList = $("ul");
+  const tagItemList = $$("li");
+  let tagValueList = [];
+
+  tagItemList.forEach((tag) => {
+    tagValueList.push(tag.childNodes[TAGCONTENT].innerText);
+  });
+
+  if (e.key == "Enter") {
+    if (tagValueList.includes(e.target.value)) {
+      alert("이미 작성된 태그입니다");
+    } else if (e.target.value != "") {
+      const li = document.createElement("li");
+      li.innerHTML = `<span>${e.target.value}
+              </span> <span class="tag__cancleBtn material-icons"> cancel </span>
+            `;
+      tagList.appendChild(li);
+      e.target.value = "";
+      li.childNodes[BUTTON].addEventListener("click", removeTag);
+    } else {
+      alert("태그명을 작성해주세요");
+    }
+  }
+}
 
 function attachEvent({ input, tagBtnList }) {
   input.addEventListener("keyup", addInputTag);
