b013faa840f170c354fbd0d091f6a5a1973a705c
seohee0112 <ghdtj7@naver.com>
2022-10-11 10:21:38 +0900

    feat : 오늘, 내일 할일을 추가하는 이벤트 구현
    - input이 빈공간이면 alert창 제시
    - isToday로 구분
    - setTimeout으로 애니메이션 구현

--- week1/Happhee-To_do/main.js
+++ week1/Happhee-To_do/main.js
@@ -8,9 +8,38 @@ const BUTTON = 1;
 
 const todoLists = $$(".todo__list");
 
+function isToday(name) {
+  return name == "todo__today__info" ? TODAY : TOMORROW;
+}
+
 function removeTotoList(e) {}
 
-function addTodoList(e) {}
+// 폼 submit을 통해 리스트를 추가하고, 애니메이션으로 사용자에게 어떤 리스트가 추가되었는지 보여주기
+function addTodoList(e) {
+  e.preventDefault();
+
+  const currentForm = e.target;
+  const current = isToday(currentForm.name);
+
+  let todoList = todoLists[current];
+  const addBtn = currentForm[BUTTON];
+  const input = currentForm[INPUT];
+
+  const li = document.createElement("li");
+  if (input.value != "") {
+    li.innerHTML = `<span>${input.value}
+              </span> <span class="todo__list__cancleBtn material-icons"> cancel </span>
+            `;
+    todoList.appendChild(li);
+    input.value = "";
+    setTimeout(() => {
+      li.classList.add("swing");
+    }, 10);
+    li.classList.remove("swing");
+  } else {
+    alert("할일을 작성해주세요");
+  }
+}
 
 function selectTodoType(e) {}
 // 이벤트 부착
