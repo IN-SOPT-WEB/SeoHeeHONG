b7c4820b521e53310703c96b7ee181188cca21a1
seohee0112 <ghdtj7@naver.com>
2022-10-12 11:04:36 +0900

    feat : 로컬스토리지 생성, 제거 구현

--- week1/Happhee.log/writePage/write.js
+++ week1/Happhee.log/writePage/write.js
@@ -4,15 +4,41 @@ const $ = (selector) => document.querySelector(selector);
 const BUTTON = 2;
 const TAGCONTENT = 1;
 
+const tagList = $("ul");
+
+function setLocalStorage(tagList) {
+  localStorage.setItem("tagList", JSON.stringify(tagList));
+}
+function initTagList() {
+  const currentStorage = JSON.parse(localStorage.getItem("tagList"));
+  console.log(currentStorage);
+  currentStorage.forEach((tag) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<span>${tag}
+              </span> <span class="tag__cancleBtn material-icons"> cancel </span>
+            `;
+    tagList.appendChild(li);
+    li.childNodes[BUTTON].addEventListener("click", removeTag);
+  });
+}
+
 function removeTag(e) {
   const removeLi = e.target.parentNode;
   const removeBtn = e.target;
   const isRemove = confirm("정말 삭제하시겠습니까?");
-  if (isRemove) removeLi.remove();
+  if (isRemove) {
+    removeLi.remove();
+    const currentStorage = JSON.parse(localStorage.getItem("tagList"));
+    console.log(removeLi.childNodes[0].innerText);
+    currentStorage.splice(
+      currentStorage.indexOf(removeLi.childNodes[0].innerText),
+      1
+    );
+    setLocalStorage(currentStorage);
+  }
 }
 
 function addInputTag(e) {
-  const tagList = $("ul");
   const tagItemList = $$("li");
   let tagValueList = [];
 
@@ -24,11 +50,19 @@ function addInputTag(e) {
     if (tagValueList.includes(e.target.value)) {
       alert("이미 작성된 태그입니다");
     } else if (e.target.value != "") {
+      const currentStorage = JSON.parse(localStorage.getItem("tagList"));
+
       const li = document.createElement("li");
-      li.innerHTML = `<span>${e.target.value}
-              </span> <span class="tag__cancleBtn material-icons"> cancel </span>
+      li.innerHTML = `<span>${e.target.value}</span> <span class="tag__cancleBtn material-icons"> cancel </span>
             `;
       tagList.appendChild(li);
+
+      if (!currentStorage.length) {
+        setLocalStorage([e.target.value]);
+      } else {
+        console.log(e.target.value);
+        setLocalStorage([...currentStorage, e.target.value]);
+      }
       e.target.value = "";
       li.childNodes[BUTTON].addEventListener("click", removeTag);
     } else {
@@ -46,6 +80,8 @@ function attachEvent({ input, tagBtnList }) {
 }
 
 window.onload = () => {
+  if (localStorage.getItem("tagList")) initTagList();
+
   attachEvent({
     input: $("input"),
     tagBtnList: $$(".tag__cancleBtn"),
