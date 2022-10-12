53eef22bd348349bdaf98862b09900d76fead5d7
seohee0112 <ghdtj7@naver.com>
2022-10-12 14:17:11 +0900

    feat : 슬라이더 함수 구현

--- week1/Happhee.log/mainPage/main.js
+++ week1/Happhee.log/mainPage/main.js
@@ -6,6 +6,11 @@ const ICON = "expand_more";
 const velogTimeBtn = $(".time__btn");
 const velogTimeList = $$(".time__item");
 
+const section = $("section");
+const velogLength = $$(".velog").length - 2;
+const CARD_WIDTH = 400;
+let currentSlide = 1;
+
 function selectVelogTime(e) {
   const selectTime = e.target.innerText;
   velogTimeBtn.value = selectTime;
@@ -34,13 +39,61 @@ function toggleVelogTimeMenu() {
     timeTypeUl.classList.remove("hide");
   else timeTypeUl.classList.add("hide");
 }
-function attachEvent() {
+function openModalArticle(e) {
+  console.log(e.currentTarget);
+  const modalBody = $(".modal__body");
+  const modal = $(".modal");
+
+  modalBody.innerHTML = e.currentTarget.innerHTML;
+  modal.classList.remove("hide");
+}
+function closeModalArticle() {
+  const modal = $(".modal");
+  modal.classList.add("hide");
+}
+
+const slidePrevious = () => {
+  if (currentSlide === 1) {
+    currentSlide = velogLength;
+  } else {
+    currentSlide--;
+  }
+  section.style.transform = `translateX(-${CARD_WIDTH * (currentSlide - 1)}px)`;
+};
+
+//캐러셀 슬라이드 오른쪽으로 이동
+const slideNext = () => {
+  if (currentSlide >= velogLength) {
+    currentSlide = 0;
+  }
+  section.style.transform = `translateX(-${CARD_WIDTH * currentSlide}px)`;
+  currentSlide += 1;
+};
+
+function attachEvent({
+  velogArticleList,
+  modalCancelBtn,
+  previousBtn,
+  nextBtn,
+}) {
   velogTimeBtn.addEventListener("click", toggleVelogTimeMenu);
   velogTimeList.forEach((velogTime) => {
     velogTime.addEventListener("click", selectVelogTime);
   });
+  velogArticleList.forEach((velogArticle) => {
+    velogArticle.addEventListener("click", openModalArticle);
+  });
+  modalCancelBtn.addEventListener("click", closeModalArticle);
+
+  previousBtn.addEventListener("click", slidePrevious);
+  nextBtn.addEventListener("click", slideNext);
 }
 
 window.onload = () => {
-  attachEvent();
+  attachEvent({
+    velogArticleList: $$(".velog"),
+    modalCancelBtn: $(".modal__cancleBtn"),
+    previousBtn: $(".prev_btn"),
+    nextBtn: $(".next_btn"),
+  });
 };
