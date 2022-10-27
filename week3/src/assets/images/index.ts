03752c3489e204624067fcee8fa13aae857e3ef3
seohee0112 <ghdtj7@naver.com>
2022-10-28 07:57:05 +0900

    feat : ✨ 모달 컨텐트구현

--- week3/src/assets/images/index.ts
+++ week3/src/assets/images/index.ts
@@ -8,21 +8,26 @@ export const pokemonItems: PokemonItem[] = [
   {
     name: '꼬부기',
     url: image1,
+    modalContent: '꼬부꼬부~✨',
   },
   {
     name: '수댕이',
     url: image2,
+    modalContent: '수댕수댕💦💦',
   },
   {
     name: '이브이',
     url: image3,
+    modalContent: '에뽀이🍁🍁',
   },
   {
     name: '토케피',
     url: image4,
+    modalContent: '토키~풀잎🌱🌱',
   },
   {
     name: '피카츄',
     url: image5,
+    modalContent: '피카피카~⚡️⚡️',
   },
 ];
--- week3/src/types/image.ts
+++ week3/src/types/image.ts
@@ -1,4 +1,5 @@
 export interface PokemonItem {
   name: string;
   url: string;
+  modalContent: string;
 }
