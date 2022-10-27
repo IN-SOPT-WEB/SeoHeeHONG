43ef1b0e866377af4dd255e25747b0e3fa450713
seohee0112 <ghdtj7@naver.com>
2022-10-28 03:26:21 +0900

    css : 🎨 이미지 타입 및 모델적용

---
+++ week3/src/assets/images/index.ts
@@ -0,0 +1,28 @@
+import { PokemonItem } from '../../types/image';
+import image1 from './꼬부기.gif';
+import image2 from './수댕이.gif';
+import image3 from './이브이.gif';
+import image4 from './토케피.gif';
+import image5 from './피카츄.gif';
+export const pokemonItems: PokemonItem[] = [
+  {
+    name: '꼬부기',
+    url: image1,
+  },
+  {
+    name: '수댕이',
+    url: image2,
+  },
+  {
+    name: '이브이',
+    url: image3,
+  },
+  {
+    name: '토케피',
+    url: image4,
+  },
+  {
+    name: '피카츄',
+    url: image5,
+  },
+];
