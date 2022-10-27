362e6f7063b2548be3623ec0e1136062b8b60ebc
seohee0112 <ghdtj7@naver.com>
2022-10-28 00:27:23 +0900

    feat : ✨ 라우터 구현

---
+++ week3/src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import Router from './common/Router';
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </>
+  );
+}
+export default App;
