d4cabfb9ec25f8bf42c74429c26ad3b905897c0a
seohee0112 <ghdtj7@naver.com>
2022-11-10 08:27:34 +0900

    feat : 검색 기록 UI구성 및 props 전달 받기 구현

---
+++ week4/src/components/Histroy.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { UserProfile, UserProfileStatus } from '../types';
+import { getUser } from '../utils/lib/api';
+
+interface HistoryProps {
+  historyList: string[];
+  handleHistoryList: (newHistoryList: string[]) => void;
+  userProfile: UserProfile;
+  handleUserProfile: (newUserProfile: UserProfile, newStatus: UserProfileStatus) => void;
+  isHistory: boolean;
+  handleIsHistory: () => void;
+}
+const Histroy = (props: HistoryProps) => {
+  const { historyList, handleHistoryList, userProfile, handleUserProfile, isHistory, handleIsHistory } = props;
+
+  const selectUserProfile = async (e: React.MouseEvent<HTMLParagraphElement>) => {};
+
+  const deleteUserProfile = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  return (
+    <StUl>
+      {isHistory ? (
+        historyList.length === 0 ? (
+          <p></p>
+        ) : (
+          historyList.map((history) => (
+            <StLi key={history}>
+              <p role="presentation" onClick={selectUserProfile}>
+                {history}
+              </p>
+              <button type="button" onClick={deleteUserProfile}>
+                X
+              </button>
+            </StLi>
+          ))
+        )
+      ) : (
+        <></>
+      )}
+    </StUl>
+  );
+};
+const StUl = styled.ul`
+  position: absolute;
+  top: 19%;
+  left: 13rem;
+
+  width: 50%;
+
+  border-radius: 0.1rem;
+  text-decoration: none;
+  z-index: 5;
+
+  background-color: ${({ theme }) => theme.colors.yellow};
+  color: ${({ theme }) => theme.colors.purple};
+`;
+const StLi = styled.li`
+  padding: 0.8rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > button {
+    padding: 0.4rem;
+    border: 0.1rem;
+    outline: 0;
+    background-color: ${({ theme }) => theme.colors.pink};
+
+    color: ${({ theme }) => theme.colors.white};
+    cursor: pointer;
+  }
+  & > p {
+    width: 100%;
+    cursor: pointer;
+    &:hover {
+      color: ${({ theme }) => theme.colors.pink};
+    }
+  }
+`;
+export default Histroy;
